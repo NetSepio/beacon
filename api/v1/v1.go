1b7a4264c9888e23a9f7d1f7b588e6c444ba88cb
vaibhavvvvv <vaibhavng7@gmail.com>
2025-04-09 04:13:47 +0530

    Create Beacon Node

---
+++ api/v1/v1.go
@@ -0,0 +1,21 @@
+package v1
+
+import (
+	"github.com/NetSepio/erebrus/api/v1/authenticate"
+	"github.com/NetSepio/erebrus/api/v1/client"
+	"github.com/NetSepio/erebrus/api/v1/server"
+	"github.com/NetSepio/erebrus/api/v1/status"
+
+	"github.com/gin-gonic/gin"
+)
+
+// ApplyRoutes Setup API EndPoints
+func ApplyRoutes(r *gin.RouterGroup) {
+	v1 := r.Group("/v1.0")
+	{
+		client.ApplyRoutes(v1)
+		server.ApplyRoutes(v1)
+		status.ApplyRoutes(v1)
+		authenticate.ApplyRoutes(v1)	
+	}
+}
